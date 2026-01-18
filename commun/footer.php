3ced06fe8ade4812f90106e59419da6bf118de99
elysaph <elysaph@gmail.com>
2026-01-18 14:46:44 +0100

    travail ensemble avec Rita sur structure html et php

--- commun/footer.php
+++ commun/footer.php
@@ -1,20 +1,21 @@
-<?php include '../commun/footer.php'; ?>
-<footer class="bg-dark text-white py-4 mt-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="../assets/img/logo-ecole.png" alt="Logo École" height="60" class="img-fluid">
-                    <h5>Club Sportif</h5>
-                    <p>Club multi-sports : basketball, tennis, volleyball.</p>
-                    <p>© 2026 Tous droits réservés.</p>
-                </div>
-                <div class="col-md-3 text-md-end">
-                    <p><strong>Contact :</strong> 01 23 45 67 89</p>
-                    <p>contact@clubsportif.fr</p>
-                </div>
+<footer class="bg-white text-black py-4 mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img src="../img/logocfa.png" alt="Logo École" height="50" width="125" margin_bottom="30px" class="img-fluid">
+            </div>
+            <div class="col-md-4">
+                <h5>Club Sportif</h5>
+                <p>Club multi-sports : basketball, tennis, volleyball.</p>
+                <p>© 2026 Tous droits réservés.</p>
+            </div>
+            <div class="col-md-4 text-md-start">
+                <p><strong>Contact :</strong> 01 23 45 67 89</p>
+                <p><strong>E-mail: </strong> contact@clubsportif.fr</p>
             </div>
         </div>
-    </footer>
-    
+    </div>
+</footer>
+
 </body>
 </html>
